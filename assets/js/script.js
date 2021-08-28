dc7aac819add0e21d02d1cbc02254ce5c7e4118c
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-08-28 07:13:03 -0400

    code refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,6 @@ var printTasks = function () {
 
         var taskP = $("<p>").addClass("description task-item-" + list).text(arr)
 
-        // console.log(list)
-        // console.log(taskP);
 
         $("#task-item-" + list).replaceWith(taskP);
     })
@@ -31,7 +29,7 @@ var printTasks = function () {
 
 
 
-//color code hours bins
+//color code hours 
 var hourAudit = function () {
     var currentHour = moment().hour()
 
@@ -49,7 +47,6 @@ var hourAudit = function () {
 
 //Task update with click
 $(".taskBin").on("click", "p", function () {
-    // console.log("<p> was clicked");
     var text = $(this)
         .text()
         .trim();
@@ -63,26 +60,21 @@ $(".taskBin").on("click", "p", function () {
 
 //Task needs to be updated
 $(".taskBin").on("blur", "textarea", function () {
-    //get the textareas; current value/text
     var text = $(this)
         .val()
         .trim();
-    // console.log(text)
 
     //recreate p element
     var taskP = $("<p>")
         .addClass("taskItem")
         .text(text);
 
-    // replace textarea with p element
     $(this).replaceWith(taskP);
 });
 
 //Save tasks
 $(".saveBtn").on("click", function () {
-    //   console.log("<save button> was clicked");
     var index = $(".saveBtn").index(this);
-    //   console.log(index)
     tasks[index] = $(this).parent().find(".taskItem").text();
     localStorage.setItem("tasks", JSON.stringify(tasks));
 });
